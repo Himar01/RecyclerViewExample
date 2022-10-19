f8446b3c213ab62739dc40129653bf853ba81297
himar01 <himarmoreno01@gmail.com>
2022-10-19 01:26:18 +0100

    Avanzando hasta el 20:29

--- app/src/main/java/es/ulpgc/moreno101/himar/recyclerviewexample/adapter/SuperAppAdapter.kt
+++ app/src/main/java/es/ulpgc/moreno101/himar/recyclerviewexample/adapter/SuperAppAdapter.kt
@@ -1,12 +1,15 @@
 package es.ulpgc.moreno101.himar.recyclerviewexample.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import es.ulpgc.moreno101.himar.recyclerviewexample.R
 import es.ulpgc.moreno101.himar.recyclerviewexample.SuperApp
 
 class SuperAppAdapter(val superAppList: List<SuperApp>) : RecyclerView.Adapter<SuperAppViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperAppViewHolder {
-
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SuperAppViewHolder(layoutInflater.inflate(R.layout.item_sample, parent, false));
     }
 
     override fun onBindViewHolder(holder: SuperAppViewHolder, position: Int) {
--- app/src/main/java/es/ulpgc/moreno101/himar/recyclerviewexample/adapter/SuperAppViewHolder.kt
+++ app/src/main/java/es/ulpgc/moreno101/himar/recyclerviewexample/adapter/SuperAppViewHolder.kt
@@ -1,6 +1,13 @@
 package es.ulpgc.moreno101.himar.recyclerviewexample.adapter
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import es.ulpgc.moreno101.himar.recyclerviewexample.SuperApp
 
 
-class SuperAppViewHolder {
+class SuperAppViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    fun render(superApp: SuperApp) {
+
+    }
 }
